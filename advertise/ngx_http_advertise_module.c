fd798b4c874a14d4008758dd4067d3fbe4a28318
gfleury <gfleury@gmail.com>
2016-07-04 14:27:44 -0300

    Fix so_reuseaddr

--- advertise/ngx_http_advertise_module.c
+++ advertise/ngx_http_advertise_module.c
@@ -591,7 +591,7 @@ static ngx_int_t ma_group_join(const u_char *addr, ngx_int_t port, const u_char 
     setsockopt(ma_mgroup_socket, IPPROTO_IP, IP_MULTICAST_LOOP,
                        &one, sizeof(unsigned char));
 
-    if ((rv = ngx_mcast_join(&ma_mgroup_socket, &ma_mgroup_sa, &ma_niface_sa, NULL)) != NGX_OK) {
+    /*if ((rv = ngx_mcast_join(&ma_mgroup_socket, &ma_mgroup_sa, &ma_niface_sa, NULL)) != NGX_OK) {
         ngx_log_error(NGX_LOG_CRIT, cf->log, 0,
                 "mod_advertise: ma_group_join apr_mcast_join failed");
 
@@ -601,7 +601,7 @@ static ngx_int_t ma_group_join(const u_char *addr, ngx_int_t port, const u_char 
             close(ma_mgroup_socket);
             return rv;
         }
-    }
+    }*/
 
     if ((rv = ngx_mcast_hops(ma_mgroup_socket, MA_ADVERTISE_HOPS)) != NGX_OK) {
         ngx_log_error(NGX_LOG_CRIT, cf->log, 0,
